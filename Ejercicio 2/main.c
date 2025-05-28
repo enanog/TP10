7c9994dd7721ec2e3ff5c60bb0ca2cf2ab1f0eef
dyfrigerio <dyfrigerio@AWSNV-L-VDA07.itbadc.itba.edu.ar>
2025-05-28 10:49:28 -0300

    Programa 2 terminado

--- Ejercicio 2/main.c
+++ Ejercicio 2/main.c
@@ -5,4 +5,32 @@
  *      Author: dyfrigerio
  */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
+float pseudoRand(float n, float m);
+
+int main(void)
+{
+	printf("%f", pseudoRand(0.0, 1000.0));
+	return 0;
+}
+
+float pseudoRand(float n, float m)
+{
+	srand(time(NULL));
+	float resp = rand();
+	while(resp < n || resp > m)
+	{
+		if(resp < n)
+		{
+			resp *= 2;
+		}
+		if(resp > m)
+		{
+			resp /= 2;
+		}
+	}
+	return resp;
+}
